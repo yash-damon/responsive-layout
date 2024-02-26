a899c074fd8cb8a2b1173d41b57a534609e0f9cb
yash-damon <yeswanthreddy@Damon.local>
2024-02-26 21:36:56 +0530

    added the comments to check

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const navbarlinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click',() => {
     navbarlinks.classList.toggle('active')
 })
-
+//this is a commit to know its working or not
